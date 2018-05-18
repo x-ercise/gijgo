ba5fb451ba060ee173c3345d76fd56808014e019
PanupongK <x.fronzen@gmail.com>
2018-05-19 00:44:58 +0700

    create message.th-th.js
    for improve Thai language

---
+++ src/grid/js/messages/message.th-th.js
@@ -0,0 +1,21 @@
+gj.grid.messages['th-th'] = {
+    First: 'หน้าแรก',
+    Previous: 'หน้าล่าสุด',
+    Next: 'ถัดไป',
+    Last: 'สุดท้าย',
+    Page: 'หน้า',
+    FirstPageTooltip: 'หน้าแรก',
+    PreviousPageTooltip: 'หน้าก่อนนี้',
+    NextPageTooltip: '',หน้าถัดไป
+    LastPageTooltip: 'หน้าสุดท้าย',
+    Refresh: 'Refresh',
+    Of: 'ของ',
+    DisplayingRecords: 'กำลังแสดงผลรายการ',
+    RowsPerPage: 'แถวต่อหน้า:',
+    Edit: 'แก้ไข',
+    Delete: 'ลบ',
+    Update: 'ดำเนินการแก้',
+    Cancel: 'ยกเลิก',
+    NoRecordsFound: 'ไม่พบข้อมูล.',
+    Loading: 'โหลดข้อมูล...'
+};
